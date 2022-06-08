f28f186fe83364716c9cda9498a242b1b340a110
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-06-07 22:57:20 -0700

    Adding changes to the JS functions

--- Assets/JS/weather.js
+++ Assets/JS/weather.js
@@ -1,6 +1,8 @@
 
 var apiKey = '9c522f3da8ba41a607c0a5a49b9d3c03';
-
+var inputVal = input.value;
+var currentDate = moment().format('MM/DD/YY');
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 geoCode('Palo Alto');
 
 function geoCode(city) {
@@ -29,22 +31,49 @@ function weatherSearch(lon, lat) {
     })
 };
 
-function displayWeather(data, city) {
+$('#search-btn').on('submit', () => {
+    let long;
+    let lat;
 
-};
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=> {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const base = `https://api.openweathermap.org/data/2.5/weatherlat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+            console.log(base);
+        fetch(base).then((response) => {
+        return response.json();
+        })
+        .then((data) => {
+            const { temp } = data.main;
+            const place = data.name;
+            const { description, icon } = data.weather[0];
+            let foreCard = $('#forecast')
+            let foreImg =$('<img>').attr('src', iconLink);
+            let foreTempH = $('<p>').text(`High: ${data.daily[i].temp.max} \xB0F`);
+            let foreTemL = $('<p>').text(`Low: ${data.daily[i].temp.min} \xB0F`);
+            let foreWind = $('<p>').text(`Wind: ${data.daily[i].wind_speed} \MPH`);
+            let foreHumidity =  $('<p>').text(`Low: ${data.daily[i].humdity}%`);
+            foreCard.append(foreDate, foreImg, foreTemp, foreTempL, foreWind, foreHumidity);
+            foreDiv.append(foreCard);
+ 
+        });
+    });
+    }
+});
 
-
-
-$('#search-btn').on('click', fucntion(event) {
-    event.preventDeafult();
-    let city = $('#city-search').val().trim().split(' ');
-    for (i = 0, i < city.length; i ++) {
-        city[i] = city[i][0].toUpperCase() + city[i].substr(1); 
-       };
-    city = city.join(' ');
-    $('#city-serarch').val(' ');
+$('#search-btn').on('click', function(event) {
+    event.preventDefault();
+    let city = $('#search-input').val().trim().split(' ');
+    for (i=0; i < city.length; i++) {
+        city[i] = city[i][0].toUpperCase() + city[i].substr(1)
+    };
+    city = city.join('');
     geoCode(city)
 });
+
+
+
 
 
 
