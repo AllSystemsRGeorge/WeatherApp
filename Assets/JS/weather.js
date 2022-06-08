4f5f4d010a2162816d6dfa556bc200d3426d3661
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-06-07 23:19:06 -0700

    Cleaning up JS

--- Assets/JS/weather.js
+++ Assets/JS/weather.js
@@ -1,8 +1,6 @@
 
 var apiKey = '9c522f3da8ba41a607c0a5a49b9d3c03';
-var inputVal = input.value;
 var currentDate = moment().format('MM/DD/YY');
-var url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 geoCode('Palo Alto');
 
 function geoCode(city) {
@@ -49,13 +47,18 @@ $('#search-btn').on('submit', () => {
             const place = data.name;
             const { description, icon } = data.weather[0];
             let foreCard = $('#forecast')
+            let container = $('#today')
+            let cityName = $('<h2>').text(`${data.city + currentDate}`)
             let foreImg =$('<img>').attr('src', iconLink);
             let foreTempH = $('<p>').text(`High: ${data.daily[i].temp.max} \xB0F`);
             let foreTemL = $('<p>').text(`Low: ${data.daily[i].temp.min} \xB0F`);
             let foreWind = $('<p>').text(`Wind: ${data.daily[i].wind_speed} \MPH`);
             let foreHumidity =  $('<p>').text(`Low: ${data.daily[i].humdity}%`);
+            container.append(cityName, foreImg, foreTemp, foreWind, foreHumidity)
             foreCard.append(foreDate, foreImg, foreTemp, foreTempL, foreWind, foreHumidity);
             foreDiv.append(foreCard);
+
+            localStorage.setItem('city', JSON.stringify(data.city));
  
         });
     });
@@ -74,34 +77,4 @@ $('#search-btn').on('click', function(event) {
 
 
 
-
-
-
-// display Weather Function
-
-// function to handle displaying all the weather
-// to empty the container div when entering the html
-// display current weather (in its own container)
-// date, icon, temp, wind, humditiy, uv index (color)
-// target div to put the current weather in
-// create elements and add the data from the fetch data
-// Append the elements to the appropriate div
-
-// 5 day forecast
-// For loop
-// Display 
-
-// $("<p>').text(`Temp: ${data.current.temp} \xB0F`);
-// let UVI = data.current.uvi;
-// if (UVI < 3) {
-    // level = 'low'
-
-// search button event
-// prevent default
-// value from the search input and store it in a variable
-// value of the input back into an empty string
-// call your first api to start the search
-
-// function to handle local storage
-// functon to handle 
 
